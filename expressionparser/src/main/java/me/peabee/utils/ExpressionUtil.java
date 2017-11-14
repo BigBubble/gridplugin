3c4617e1f87229bce69748a39abc5bcd4999a0ac
pengbo <pengbowo@126.com>
2017-11-14 16:42:53 +0800

    语法校验

---
+++ expressionparser/src/main/java/me/peabee/utils/ExpressionUtil.java
@@ -0,0 +1,14 @@
+package me.peabee.utils;
+
+/**
+ * Created by pengbo on 17-11-9.
+ */
+public class ExpressionUtil {
+    /**
+     * 表达式语法检查
+     * @param expression
+     */
+    public static void syntaxCheck(String expression){
+        //1 主表达式逻辑分组
+    }
+}
